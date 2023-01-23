44e71c0facbac0cbf2d87d25e089a1654b993111
Patrick <ich@patrickdavid.de>
2023-01-23 12:41:21 +0100

    Sha-Bang hinzugefügt

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
+#!/bin/python3
 
 print('Hello World!')
+print('Bitte geben Sie etwas ein')
+blubb = input()
+print('Sie gaben ein: ' + blubb + ',was irgendwie nicht sinnvoll ist :-)')
